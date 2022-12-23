3a9edab94eb12a595886aa2381c428ed7ef16014
erandj <neizvest000@gmail.com>
2022-12-24 00:14:04 +0900

    4

--- main.py
+++ main.py
@@ -1,16 +1,91 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow
+import MainWindow
+import AccountWindow
+import RegistrationWindow
 
 
-# Press the green button in the gutter to run the script.
+class Main(QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui = MainWindow.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.account = account
+
+        self.ui.pushButton.clicked.connect(self.MoveToAccountWindow)
+
+    def MoveToAccountWindow(self):
+        if self.account != '':
+            self.newWindow = Account()
+            self.newWindow.show()
+            self.close()
+        else:
+            self.newWindow = Registration()
+            self.newWindow.show()
+            self.close()
+
+
+class Registration(QMainWindow):
+    def __init__(self):
+        super(Registration, self).__init__()
+        self.ui = RegistrationWindow.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.number = ''
+
+        self.ui.pushButton_4.clicked.connect(self.MoveBack)
+        self.ui.pushButton.clicked.connect(self.CheckNumber)
+
+    def MoveBack(self):
+        self.newWindow = Main()
+        self.newWindow.show()
+        self.close()
+
+    def CheckNumber(self):
+        global account
+
+        self.number = self.ui.lineEdit.text()
+        self.ui.errorLabel.setText("")
+
+        if self.number == '':
+            self.ui.errorLabel.setText("Напишите свой номер!")
+            return
+        if self.number[0] != '8' or len(self.number) < 10:
+            self.ui.errorLabel.setText("Проверьте правильность номера!")
+            return
+
+        account = self.number
+
+        self.newWindow = Account()
+        self.newWindow.show()
+        self.close()
+
+
+class Account(QMainWindow):
+    def __init__(self):
+        super(Account, self).__init__()
+        self.ui = AccountWindow.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        if account == '':
+            self.newWindow = Registration()
+            self.newWindow.show()
+            self.close()
+
+        if len(account) == 11:
+            self.account = account[0] + " (" + account[1:4] + ") " + account[4:7] + "-" + account[7:9] + "-" + account[9:11]
+        elif len(account) == 12:
+            self.account = account[0] + " (" + account[1:5] + ") " + account[5:8] + "-" + account[8:10] + "-" + account[10:12]
+
+        self.ui.numberLabel.setText(self.account)
+
+
+account = ''
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QtWidgets.QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
